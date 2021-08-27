6688c0d889baa78e37d73f91cf37d206347e408c
UThemes <86716632+ulathemes@users.noreply.github.com>
2021-08-27 17:20:38 +0530

    Create my-gplastra.php

---
+++ my-gplastra.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: My GPLAstra
+ * Plugin URI: https://www.gplastra.com
+ * Description: A Special & Custom Plugin for GPLAstra to add custom snippets, codes or functions.
+ * Version: 1.0
+ * Author: GPLAstra
+ * Author URI: https://www.gplastra.com
+ */
+
+// Display Shortcode Content By Replacing WooCommerce Short Description
+
+add_filter('woocommerce_short_description', function ($description) {
+	if (! is_product()) { return; }   
+	return do_shortcode('[mbv name="product-info"]');
+});
