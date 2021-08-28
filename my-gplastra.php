d73ab110cfa2488e8b51e8511b7536bc338e48c4
UThemes <86716632+ulathemes@users.noreply.github.com>
2021-08-28 18:54:09 +0530

    Update my-gplastra.php

--- my-gplastra.php
+++ my-gplastra.php
@@ -14,3 +14,13 @@ add_filter('woocommerce_short_description', function ($description) {
 	if (! is_product()) { return; }   
 	return $description.do_shortcode('[mbv name="product-info"]');
 });
+
+// Enable Gutenberg in WooCommerce
+function activate_gutenberg_product( $can_edit, $post_type ) {
+
+    if ( $post_type == 'product' ) {
+        $can_edit = true;
+    }
+    return $can_edit;
+}
+add_filter( 'use_block_editor_for_post_type', 'activate_gutenberg_product', 10, 2 );
