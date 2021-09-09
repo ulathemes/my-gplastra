64446ea55bc04f995e3624c19c9082dad079e890
UThemes <86716632+ulathemes@users.noreply.github.com>
2021-09-09 18:04:19 +0530

    Delete guten-options.php

--- guten-options.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Disable Gutenberg in Widgets
-add_filter( 'gutenberg_use_widgets_block_editor', '__return_false' );
-	add_filter( 'use_widgets_block_editor', '__return_false' );
-
-// Disable Jetpack Blocks in Gutenberg 
-add_filter( 'jetpack_gutenberg', '__return_false' );
